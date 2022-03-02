a8f05a78c880def6fd17f7d4768836a67984d923
goszowski <goszowski@gmail.com>
2022-03-02 17:28:12 +0200

    kommersant

--- config/bomb.php
+++ config/bomb.php
@@ -4,16 +4,11 @@ return [
 
     'enabled' => env('BOMB_ENABLED'),
 
-    'theards' => 1000,
+    'theards' => 10000,
 
-    'timeout' => 3,
+    'timeout' => 1,
 
     'destinations' => [
-        ['url'=> 'ns1.ddos-guard.net', 'method'=> 'get'],
-        ['url'=> 'ns2.ddos-guard.net', 'method'=> 'get'],
-        ['url'=> 'ns3.ddos-guard.net', 'method'=> 'get'],
-        ['url'=> 'ns4.ddos-guard.net', 'method'=> 'get'],
-        ['url'=> 'ns5.ddos-guard.net', 'method'=> 'get'],
-        ['url'=> 'ns6.ddos-guard.net', 'method'=> 'get'],
+        ['url'=> 'https://www.kommersant.ru', 'method'=> 'get'],
     ],
 ];
